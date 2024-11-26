87bc08015f5a5c13bf0889f64c70020546e6922b
Samiur Prapon <samiur.prapon@gmail.com>
2024-11-26 09:13:01 +0600

    feat: Implement 'Load More' functionality for posts

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { Loader } from 'lucide-react';
+
 import Hero from '~/components/HeroSection';
 import Navigation from '~/components/Navigation';
 import PostCard from '~/components/PostCard';
@@ -6,6 +9,18 @@ import { Sidebar } from '~/components/Sidebar';
 import fakePosts from '~/utils/fake';
 
 function HomePage() {
+	const [visiblePosts, setVisiblePosts] = useState<number>(3);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	const handleLoadMore = () => {
+		setIsLoading(true);
+
+		setTimeout(() => {
+			setVisiblePosts(visiblePosts + 3);
+			setIsLoading(false);
+		}, 600);
+	};
+
 	return (
 		<>
 			<Navigation />
@@ -19,10 +34,20 @@ function HomePage() {
 								<h2 className="post-container__title">Latest Articles</h2>
 
 								<div className="post-container__grid">
-									{fakePosts.map((post, index) => (
+									{fakePosts.slice(0, visiblePosts).map((post, index) => (
 										<PostCard post={post} key={index} />
 									))}
 								</div>
+
+								{visiblePosts < fakePosts.length && (
+									<button
+										className="post-container__load-more"
+										onClick={handleLoadMore}
+										disabled={isLoading} // Disable button while loading
+									>
+										{isLoading ? <Loader /> : 'Load More'}
+									</button>
+								)}
 							</div>
 						</div>
 						<div className="col-md-3">
