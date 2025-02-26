481ef133b498ccdacff0e32518ac0a9feaab793c
Samiur Prapon <samiur.prapon@gmail.com>
2025-02-26 07:52:31 +0600

    feat(api): add configuration schema validation and update environment variable handling

--- apps/api/src/config/required.ts
+++ apps/api/src/config/required.ts
@@ -1,42 +1,32 @@
 import path from 'path';
 import dotenv from 'dotenv';
 
-const env = process.env.NODE_ENV || 'development'; // default to development [development, production, test]
+import { Config, configSchema } from '@/utils/validations/schemas/ConfigSchema';
+
+const env = process.env.NODE_ENV || 'development';
 const envFilePath = path.resolve(__dirname, `../../../.env.${env}.local`);
 
-// Load the .env file
 const result = dotenv.config({ path: envFilePath });
 
-/*
- * DO NOT REMOVE THIS CODE
- * This error check is important for database migration related task
- *
- */
 if (result.error) {
 	dotenv.config({
 		path: path.resolve(__dirname, `../../.env.${env}.local`),
 	});
 }
 
-export const config = {
-	NODE_ENV: process.env.NODE_ENV || 'development',
-	HOST: process.env.HOST || 'localhost',
-	PORT: +(process.env.PORT || 3001),
-
-	// Database
-	APP_DB_NAME: process.env.APP_DB_NAME || 'diary',
-	APP_DB_PASSWORD: process.env.APP_DB_PASSWORD || '',
-	APP_DB_USER: process.env.APP_DB_USER || '',
-	APP_DB_HOST: process.env.APP_DB_HOST || '',
-	APP_DB_PORT: +(process.env.APP_DB_PORT || 5432),
-	APP_DB_SSL: process.env.APP_DB_SSL || '',
-
-	// Redis
-	REDIS_URI: process.env.REDIS_URI || 'redis://localhost:6379',
-
-	// Security
-	PASSWORD_SALT: +(process.env.PASSWORD_SALT || 10),
-	ACCESS_TOKEN_EXPIRES: process.env.ACCESS_TOKEN_EXPIRES || '30m',
-};
+export const config: Config = configSchema.parse({
+	NODE_ENV: process.env.NODE_ENV,
+	HOST: process.env.HOST,
+	PORT: process.env.PORT,
+	APP_DB_NAME: process.env.APP_DB_NAME,
+	APP_DB_PASSWORD: process.env.APP_DB_PASSWORD,
+	APP_DB_USER: process.env.APP_DB_USER,
+	APP_DB_HOST: process.env.APP_DB_HOST,
+	APP_DB_PORT: process.env.APP_DB_PORT,
+	APP_DB_SSL: process.env.APP_DB_SSL,
+	REDIS_URI: process.env.REDIS_URI,
+	PASSWORD_SALT: process.env.PASSWORD_SALT,
+	ACCESS_TOKEN_EXPIRES: process.env.ACCESS_TOKEN_EXPIRES,
+});
 
 export default config;
